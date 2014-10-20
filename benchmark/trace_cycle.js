d2fbd313e736005708668f9699da04723eb68c88
inikulin <ifaaan@gmail.com>
2014-10-21 02:22:59 +0400

    Clean up project a little bit

--- benchmark/trace_cycle.js
+++ benchmark/trace_cycle.js
@@ -3,6 +3,6 @@ var asts = require('./asts'),
 
 for (var j = 0; j < 50; j++) {
     for (var i = 0; i < asts.length; i++)
-        esotope.generate(asts[i]);
+        esotope.generate(asts[0]);
 }
 
